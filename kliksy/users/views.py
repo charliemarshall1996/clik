3b411e9455d9d5e79a1f27d27da88711b6307167
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-11-13 14:29:48 +0000

    added ProfileRegistrationForm to registration_view

--- kliksy/users/views.py
+++ kliksy/users/views.py
@@ -2,15 +2,14 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, ProfileRegistrationForm
 # Create your views here.
 
 
 def registration_view(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
-
-        # TODO: Add Profile Form
+        profile_form = ProfileRegistrationForm(request.POST)
 
         if user_form.is_valid():  # noqa
 
@@ -21,7 +20,14 @@ def registration_view(request):
                 # Redirect to prevent bot resubmission
                 return redirect('home')
 
-            user_form.save()
+            user = user_form.save()
+            user.email_verified = False
+            user.save()
+
+            profile = profile_form.save()
+            profile.user = user
+            profile.clean()
+            profile.save()
 
             # TODO: Send verification email Logic
             # send_verification_email(user, request)
@@ -33,5 +39,6 @@ def registration_view(request):
             return redirect('accounts:login')
     else:
         user_form = UserRegistrationForm()
+        profile_form = ProfileRegistrationForm()
 
-    return render(request, 'accounts/register.html', {'user_form': user_form})
+    return render(request, 'accounts/register.html', {'user_form': user_form, 'profile_form': profile_form})
