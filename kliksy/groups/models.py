dcbe0574ffe06aecab94db4360dd147471c631b7
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-11-14 10:49:29 +0000

    added groups app, Groups and Events models

---
+++ kliksy/groups/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+from core.models import Categories
+from users.models import Profile
+
+
+class Groups(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(Profile, related_name='groups')
+    category = models.ManyToManyField(Categories, related_name='groups')
+
+
+class Events(models.Model):
+    group = models.ForeignKey(
+        Groups, related_name='events', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=100)
+    attendants = models.ManyToManyField(Profile, related_name='events')
