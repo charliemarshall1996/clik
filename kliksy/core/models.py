78b96d7848c64bacb739dd18c161373a8d18e379
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-11-14 10:36:54 +0000

    added Categories model

--- kliksy/core/models.py
+++ kliksy/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='subcategories'
+    )
+
+    def __str__(self):
+        return self.name
