64583893e04e838fc4c1f839c8c57c45931d0bd1
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-11-14 13:17:18 +0000

    added categories

--- kliksy/core/admin.py
+++ kliksy/core/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import Category
+
+admin.site.register(Category, MPTTModelAdmin)
--- kliksy/core/models.py
+++ kliksy/core/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
 
-
-class Categories(models.Model):
-    name = models.CharField(max_length=100)
-    parent = models.ForeignKey(
+class Category(MPTTModel):
+    name = models.CharField(max_length=255, unique=True)
+    parent = TreeForeignKey(
         'self',
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         related_name='subcategories'
     )
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
 
     def __str__(self):
         return self.name
